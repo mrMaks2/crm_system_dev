d2e04c39f2abf86b97f2fabbf4ffccb645b3bea5
mrMaks2 <shpakovmaks@gmail.com>
2025-07-14 14:55:11 +0300

    Замена SQLite на PostgreSQL

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'crm_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_project',
+        'USER': 'maks',
+        'PASSWORD': 'maks',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
